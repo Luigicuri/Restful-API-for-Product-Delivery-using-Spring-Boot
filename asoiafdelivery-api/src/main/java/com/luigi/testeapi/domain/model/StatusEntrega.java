b99f0d1a4e9bc18b10cb2181bb897be25ef41ec1
Luigi Curi <74516086+Luigicuri@users.noreply.github.com>
2021-10-27 20:15:54 -0300

    Delete asoiafdelivery-api directory

--- asoiafdelivery-api/src/main/java/com/luigi/testeapi/domain/model/StatusEntrega.java
+++
@@ -1,5 +0,0 @@
-package com.luigi.testeapi.domain.model;
-
-public enum StatusEntrega {
-	PENDENTE, FINALIZADA, CANCELADA
-}
--- asoiafdelivery-api/src/main/resources/db/migration/V001__cria-tabela-cliente.sql
+++
@@ -1,8 +0,0 @@
-create table cliente (
-id bigint not null auto_increment,
-nome varchar(60) not null,
-email varchar(200) not null,
-telefone varchar(40) not null,
-
-primary key (id)
-);
